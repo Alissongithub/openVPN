5fbcefbd9858f9a003b242070601d0de6e3052d7
Alissongithub <alissoncsbrasil@hotmail.com>
2017-07-18 14:30:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,16 +12,16 @@ wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/squidconf
 chmod +x squidconf
 ./squidconf
 wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/payloads -O /etc/payloads
-wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/alterarsenha -O /bin/alterarsenha
+wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/AlterarSenha.sh -O /bin/alterarsenha
 chmod +x /bin/alterarsenha
-wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/criarusuario -O /bin/criarusuario
+wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/criarusuario.sh -O /bin/criarusuario
 chmod +x /bin/criarusuario
-wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/mudardata -O /bin/mudardata
+wget https://raw.githubusercontent.com/Alissongithub/squid3/master/mudardata -O /bin/mudardata
 chmod +x /bin/mudardata
-wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/remover -O /bin/remover
+wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/remover.sh -O /bin/remover
 chmod +x /bin/remover
 wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/ajuda -O /bin/ajuda
 chmod +x /bin/ajuda
-wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/statusvpn -O /bin/statusvpn
+wget https://raw.githubusercontent.com/Alissongithub/openVPN/master/sshmonitor.sh -O /bin/statusvpn
 chmod +x /bin/statusvpn
 clear
